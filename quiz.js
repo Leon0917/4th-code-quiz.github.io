79a4ae62b96a141a8b500706ce080a512a135b2d
Leon Carnagie <leonfross@gmail.com>
2020-05-21 23:58:49 -0700

    Modified Javascript file

--- quiz.js
+++ quiz.js
@@ -16,63 +16,63 @@ var gameOver = false;
 // create our questions
 var questions = [
     {
-      question: "Commonly used data types DO NOT include:",
-      choiceA:"strings", 
-      choiceB:"booleans", 
-      choiceC:"alerts",
-      choiceD:"numbers",
-      answer: "C"
+        question: "Commonly used data types DO NOT include:",
+        choiceA: "strings",
+        choiceB: "booleans",
+        choiceC: "alerts",
+        choiceD: "numbers",
+        answer: "C"
     },
     {
-      question: "The condition in an if / else statement is enclosed within ____.",
-      choiceA: "quotes",
-      choiceB: "curly brackets", 
-      choiceC: "parentheses", 
-      choiceD:"square brackets",
-      answer: "C"
+        question: "The condition in an if / else statement is enclosed within ____.",
+        choiceA: "quotes",
+        choiceB: "curly brackets",
+        choiceC: "parentheses",
+        choiceD: "square brackets",
+        answer: "C"
     },
     {
-      question: "Arrays in JavaScript can be used to store ____.", 
-      choiceA: "numbers and strings",
-      choiceB:"other arrays",
-      choiceC:"booleans",
-      choiceD:"all of the above",
-      answer: "D"
+        question: "Arrays in JavaScript can be used to store ____.",
+        choiceA: "numbers and strings",
+        choiceB: "other arrays",
+        choiceC: "booleans",
+        choiceD: "all of the above",
+        answer: "D"
     },
     {
-      question: "String values must be enclosed within ____ when being assigned to variables.",
-      choiceA:"commas", 
-      choiceB:"curly brackets", 
-      choiceC:"quotes", 
-      choiceD:"parentheses",
-      answer: "C"
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        choiceA: "commas",
+        choiceB: "curly brackets",
+        choiceC: "quotes",
+        choiceD: "parentheses",
+        answer: "C"
     },
     {
-      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choiceA:"JavaScript", 
-      choiceB:"terminal / bash", 
-      choiceC:"for loop",
-      choiceD:"console.log",  
-      answer: "D"
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choiceA: "JavaScript",
+        choiceB: "terminal / bash",
+        choiceC: "for loop",
+        choiceD: "console.log",
+        answer: "D"
     }
-  ];
-  
-  // create some variables
+];
+
+// create some variables
 
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
-let count = 0;
-const questionTime = 10;
-const gaugeWidth = 150; 
-const gaugeUnit = gaugeWidth / questionTime;
+let count = 50;
+// const questionTime = 10;
+const gaugeWidth = 150;
+// const gaugeUnit = gaugeWidth / questionTime;
 let TIMER;
 let score = 0;
 
 // render a question
-function renderQuestion(){
+function renderQuestion() {
     let q = questions[runningQuestion];
-    
-    question.innerHTML = "<p>"+ q.question +"</p>";
+
+    question.innerHTML = "<p>" + q.question + "</p>";
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
@@ -89,98 +89,91 @@ function startQuiz() {
     renderQuestion();
     quiz.style.display = "block";
     renderCounter();
-    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+    TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
 }
 
 // render progress
-function renderProgress(){
-        progress.innerHTML = "<div class='prog'>question #"+(runningQuestion+1)+' of '+questions.length+  "</div>";
+function renderProgress() {
+    progress.innerHTML = "<div class='prog'>question #" + (runningQuestion + 1) + ' of ' + questions.length + "</div>";
 
 }
 // counter render
 
-function renderCounter(){
-    if(count <= questionTime){
-        counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit + "px";
-        count++
-    }
-    else {
-        count = 0;
-          answerIsWrong();
-          if(runningQuestion < lastQuestion){
-              runningQuestion++;
-              renderQuestion();
-          }else{
-              // end the quiz and show the score
-              clearInterval(TIMER);
-              scoreRender();
-          }
-    }}
+function renderCounter() {
+    counter.innerHTML = count;
+    // timeGauge.style.width = count * gaugeUnit + "px";
+    count--
 
-    function checkAnswer(answer){
-        if (gameOver === true){
-            return
-        }
-        if( answer == questions[runningQuestion].answer){
-            console.log(questions[runningQuestion])
-            // answer is correct
-            score++;
-            // change progress color to green
-            answerIsCorrect();
-        }else{
-            // answer is wrong
-            console.log(questions[runningQuestion])
-            // change progress color to red
-            answerIsWrong();
-        }
-        count = 0;
-        if(runningQuestion < lastQuestion){
-            runningQuestion++;
-            renderQuestion();
-        }else{
-            // end the quiz and show the score
-            clearInterval(TIMER);
-            scoreRender();
-            gameOver = true;
-        }
-    }
+    if (count <= 0) {
 
-    // answer is correct
-function answerIsCorrect(){
+        // end the quiz and show the score
+        clearInterval(TIMER);
+        scoreRender();
+    }
+}
+
+function checkAnswer(answer) {
+    if (gameOver === true) {
+        return
+    }
+    if (answer == questions[runningQuestion].answer) {
+        console.log(questions[runningQuestion])
+        // answer is correct
+        score++;
+        answerIsCorrect();
+    } else {
+        // answer is wrong
+        console.log(questions[runningQuestion])
+        count -= 5;  
+        answerIsWrong();
+    }
+    
+    if (runningQuestion < lastQuestion) {
+        runningQuestion++;
+        renderQuestion();
+    } else {
+        // end the quiz and show the score
+        clearInterval(TIMER);
+        scoreRender();
+        gameOver = true;
+    }
+}
+
+// answer is correct
+function answerIsCorrect() {
     document.getElementById('result').textContent = "correct";
 }
 
 // answer is Wrong
-function answerIsWrong(){
+function answerIsWrong() {
     document.getElementById('result').textContent = "wrong";
 }
 
 // score render
-function scoreRender(){
+function scoreRender() {
     scoreDiv.style.display = "block";
-    
+
     // calculate the amount of question percent answered by the user
-    var scorePerCent = Math.round(100 * score/questions.length);     
-    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";  
+    var scorePerCent = Math.round(100 * score / questions.length);
+    scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
 }
 
 function writeScore() {
-    var userInitial = document.getElementById("initial1").value 
-    const scorePercent = Math.round(100 * score/questions.length)
-    var previousUser = localStorage.getItem("userInitials")||     "welcome user"  
-    var highScore = localStorage.getItem("highScore")|| 0
+    var userInitial = document.getElementById("initial1").value
+    const scorePercent = Math.round(100 * score / questions.length)
+    var previousUser = localStorage.getItem("userInitials") || "welcome user"
+    var highScore = localStorage.getItem("highScore") || 0
     if (score > highScore) {
         localStorage.setItem("userInitials", userInitial)
         localStorage.setItem("highScore", highScore)
-        
-    } 
+
+    }
 }
-var previousUser = localStorage.getItem("userInitials")||"welcome user"  
-var highScore = localStorage.getItem("highScore")||0
+var previousUser = localStorage.getItem("userInitials") || "welcome user"
+var highScore = localStorage.getItem("highScore") || 0
 
 var localStorageDisplay = document.getElementById("store3")
-    localStorageDisplay.innerText = "previous high score:"+ highScore+ "user:" + previousUser
+localStorageDisplay.innerText = "previous high score:" + highScore + "user:" + previousUser
 
 
 
